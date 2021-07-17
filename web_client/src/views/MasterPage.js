9c80d053ac07d9976bcd751c2b6d80cb2978cc7d
Fernando Cisneros <fercr0803@gmail.com>
2021-07-17 16:04:33 -0300

    US 311: nueva hoja de estilos para la vista

--- web_client/src/views/MasterPage.js
+++ web_client/src/views/MasterPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import RegisterProduct from './RegisterProduct/RegisterProduct';
 import RegisterProductView from './RegisterProduct/RegisterProductView';
 import './MasterPage.css';
 
