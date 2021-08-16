bcdccbd7833925b552c1361325c85fb34c7aa9eb
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2021-08-16 16:51:29 -0300

    US 349: Acomodo de estilos y fondo

--- desktop_client/src/views/ChamberFlavorsDispatch/components/ChamberFlavorsDispatch.js
+++ desktop_client/src/views/ChamberFlavorsDispatch/components/ChamberFlavorsDispatch.js
@@ -4,9 +4,25 @@ import '../styles/ChamberFlavorsDispatch.css';
 export default function ChamberFlavorsDispatch() {
     return (
         <form>
-            <div className="viewTitle">
-                <h1 className="display-5">Salida de productos de cámara</h1>
+            <div className="viewContent">
+                <h1 className="display-5">Registrar salida de productos de cámara</h1>
                 <hr />
+                <div className="formRow">
+                    <div className="form-control-label">
+                        <label className='col-3' style={{fontSize:'34px'}}>Cantidad*</label>
+                    </div>
+                    <div className="form-control-input">
+                        <input type='text' className='form-control' placeholder='Ingrese nombre del producto...'></input>
+                    </div>
+                </div>
+                <div className="formRow">
+                    <div className="form-control-label">
+                        <label className='col-3' style={{fontSize:'34px'}}>Cantidad*</label>
+                    </div>
+                    <div className="form-control-input">
+                        <input type='text' className='form-control' placeholder='Ingrese nombre del producto...'></input>
+                    </div>
+                </div>
             </div>
         </form>
     );
