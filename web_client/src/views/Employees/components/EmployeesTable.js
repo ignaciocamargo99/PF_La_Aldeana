5fe2e22ffe69e22d8e7427b331199d6ab2f38f86
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2021-09-20 13:36:58 -0300

    Fecha

--- web_client/src/views/Employees/components/EmployeesTable.js
+++ web_client/src/views/Employees/components/EmployeesTable.js
@@ -58,7 +58,7 @@ export default function EmployeesTable() {
         let aux = employees;
         aux.lastName = employees.last_name;
         aux.employmentRelationship = employees.employment_relationship;
-        aux.date = moment(employees.date_admission).format('YYYY-MM-DD');
+        aux.date = moment(employees.date_admission).add(1, 'days').format('YYYY-MM-DD');
         aux.reading = true;
         setReading(aux);
         setIsReading(true);
