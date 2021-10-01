39322274c22ba8193ca979b8f0e016b5bedc61aa
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2021-09-30 23:43:37 -0300

    US 550: Input dni

--- desktop_client/src/views/EmployeeAssistance/components/EmployeeManualAssistance.js
+++ desktop_client/src/views/EmployeeAssistance/components/EmployeeManualAssistance.js
@@ -1,21 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import '../styles/ManualAssistance.css';
 import validateFloatNumbers from '../../../utils/Validations/validateFloatNumber';
+import Axios from 'axios';
+
+const PORT = require('../../../config')
 
 export default function EmployeeManualAssistance() {
+    const [isValidClass, setIsValidClass] = useState('form-control');
+    const [employee, setEmployee] = useState();
+    const [validEmployee, setValidEmployee] = useState(false);
+    const inputDNI = useRef(null);
 
-    const [isValidClass, setIsValidClass] = useState('form-control')
+    useEffect(() => {
+        Axios.get(`${PORT()}/api/employees`)
+            .then(response => setEmployee(response.data))
+            .catch(error => console.error(error))
+    }, [])
 
     const validate = (e) => {
         if (e.target.value.length > 8) e.target.value = e.target.value.slice(0, 8);
     }
 
     const onChangeButton = (e) => {
-        if(e.target.value.length === 8) setIsValidClass("form-control is-valid");
+        if (e.target.value.length === 8) setIsValidClass("form-control is-valid");
         else setIsValidClass("form-control");
     }
 
-    
+    const onClickValidation = () => {
+        let searchDNI;
+        searchDNI = employee.find((employee) => employee.dni === parseInt(inputDNI.current.value, 10));
+        console.log(searchDNI);
+        if (searchDNI) setValidEmployee((validEmployee) => {
+            return validEmployee = true
+        });
+        else setValidEmployee(false);
+        console.log(validEmployee)
+    }
 
     return (
         <>
@@ -25,10 +45,12 @@ export default function EmployeeManualAssistance() {
                     <label htmlFor="dni">DNI</label>
                 </div>
                 <div className="form-control-input">
-                    <input className={isValidClass} style={{ textAlign: 'center'}} id="dni" autoFocus type="number" placeholder="Ingrese su dni..."
-                        onKeyDown={(e) => validateFloatNumbers(e)} onInput={(e) => validate(e)} onChange={(e) => onChangeButton(e)} />
+                    <input className={isValidClass} style={{ textAlign: 'center' }} id="dni" autoFocus type="number" placeholder="Ingrese su dni..."
+                        onKeyDown={(e) => validateFloatNumbers(e)}
+                        onInput={(e) => validate(e)}
+                        onChange={(e) => onChangeButton(e)} ref={inputDNI} />
                 </div>
-                <button className="btn btn-primary" style={{marginLeft: '5px'}}>Validar</button>
+                <button className="btn btn-primary" style={{ marginLeft: '5px' }} onClick={onClickValidation}>Validar</button>
             </div>
         </>
     );
