8edeea64c2bf24a5b056898ca87b6e8deb05a9d3
Fernando Cisneros <fercr0803@gmail.com>
2021-11-03 00:08:43 -0300

    US 596: scripts para roles, permisos y permisos por rol

---
+++ server/data/incremental_updates/034_Insert_Roles.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    `ROLES` (`name`)
+VALUES
+    ('Administrador'),
+    ('Empleado');
+
+INSERT INTO
+    `DB_CHANGES`
+VALUES
+    (
+        034,
+        'Insert',
+        'Roles Administrador y Empleado.'
+    );
