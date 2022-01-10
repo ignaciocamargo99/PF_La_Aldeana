7e05ea7a38e1cd333b4606bef81041287fc832b3
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2022-01-10 19:14:20 -0300

    Script asistencia de empleados

--- server/data/incremental_updates/044_CreateTable_Assistance_Employees.sql
+++ server/data/incremental_updates/044_CreateTable_Assistance_Employees.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS `ASSISTANCE_EMPLOYEES` (
-  `id_assistance` INT NOT NULL AUTO_INCREMENT,
+  `id_assistance` int(11) NOT NULL AUTO_INCREMENT,
   `date_entry` DATETIME NOT NULL,
   `date_egress` DATETIME NOT NULL,
-  `employee` BIGINT NOT NULL
+  `employee` bigint(20) NOT NULL,
   PRIMARY KEY (`id_assistance`),
-  KEY `FK_EMPLOYEE` (`employee`),
-  CONSTRAINT `FK_EMPLOYEE` FOREIGN KEY (`employee`) REFERENCES `EMPLOYEES`(`dni`) ON DELETE CASCADE ON UPDATE NO ACTION
+  KEY `FK__EMPLOYEE` (`employee`) USING BTREE,
+  CONSTRAINT `FK__EMPLOYEE` FOREIGN KEY (`employee`) REFERENCES `EMPLOYEES`(`dni`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 INSERT INTO
   `DB_CHANGES`
 VALUES
   (
-    042,
+    044,
     'Create table',
     'Tabla de asistencia de empleados.'
   );
