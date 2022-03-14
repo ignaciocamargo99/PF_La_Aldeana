d13966d56c99e6a03d82f0e11fc700cb26e896d9
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2022-03-14 18:51:19 -0300

    US 660: Tablas permisos

---
+++ server/data/incremental_updates/066_CreateTable_Db_Accesses.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `ACCESSES` (
+    `id_access` int(11) NOT NULL AUTO_INCREMENT,
+    `name_access` VARCHAR(200) NOT NULL,
+    PRIMARY KEY (`id_access`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- INSERT INTO
+--     `DB_CHANGES`
+-- VALUES
+--     (
+--         066,
+--         'Create table',
+--         'Tabla de accesos de lectura, escritura, edición y eliminación.'
+--     );
+    
+    
+    
---
+++ server/data/incremental_updates/067_Insert_Db_Accesses.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    `ACCESSES` (`name_access`)
+VALUES
+    ('Registrar'),
+    ('Ver'),
+    ('Editar'),
+    ('Eliminar');
+
+INSERT INTO
+    `DB_CHANGES`
+VALUES
+    (
+        067,
+        'Insert',
+        'Accesos registrar, ver, editar y eliminar.'
+    );
