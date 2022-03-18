6ff81979193f66d26cc18df4328264438fd3e3fc
Joaquin Bassano <joaquinbas98@gmail.com>
2022-03-18 20:22:05 -0300

    US 721: Filtros listos

--- desktop_employee/desktop_employee/src/views/Employees/frmEmployees.cs
+++ desktop_employee/desktop_employee/src/views/Employees/frmEmployees.cs
@@ -21,6 +21,8 @@ namespace desktop_employee.src.views.Employees
         DataTable employeeSelectedTable = new DataTable();
         private Panel pnlPadre;
         config config = new();
+        DataTable employeeFilteredTable = new DataTable();
+
         public Panel PnlPadre { get => pnlPadre; set => pnlPadre = value; }
 
         public frmEmployees()
@@ -63,15 +65,16 @@ namespace desktop_employee.src.views.Employees
             {
                 btnEditEmployee.BackgroundColor = ColorTranslator.FromHtml("#383C77");
                 btnEditEmployee.TextColor = Color.White;
+                btnEditEmployee.Enabled = true;
             }
         }
 
         private DataTable createTableEmployee()
         {
             DataTable table = new DataTable();
-            DataColumn cDni = new DataColumn("dni");
-            DataColumn cName = new DataColumn("name");
-            DataColumn cLastName = new DataColumn("last_name");
+            DataColumn cDni = new DataColumn("DNI");
+            DataColumn cName = new DataColumn("NOMBRE");
+            DataColumn cLastName = new DataColumn("APELLIDO");
             table.Columns.Add(cDni);
             table.Columns.Add(cName);
             table.Columns.Add(cLastName);
@@ -88,9 +91,9 @@ namespace desktop_employee.src.views.Employees
                 if (Convert.ToInt32(arrayEmpleados[i].DNI) == dni)
                 {
                     DataRow row = employeeSelected.NewRow();
-                    row["dni"] = arrayEmpleados[i].DNI;
-                    row["name"] = arrayEmpleados[i].NOMBRE;
-                    row["last_name"] = arrayEmpleados[i].APELLIDO;
+                    row["DNI"] = arrayEmpleados[i].DNI;
+                    row["NOMBRE"] = arrayEmpleados[i].NOMBRE;
+                    row["APELLIDO"] = arrayEmpleados[i].APELLIDO;
                     employeeSelected.Rows.Add(row);
                 }
             }
@@ -116,12 +119,6 @@ namespace desktop_employee.src.views.Employees
             }
         }
 
-        private void btnAplicarFiltros_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show("Falta desarrollar los filtros");
-        }
-
-
         private void OpenForm(Form form)
         {
             for (int i = 0; i < Application.OpenForms.Count; i++)
@@ -141,5 +138,206 @@ namespace desktop_employee.src.views.Employees
             pnlPadre.Controls.Add(form);
             form.Show();
         }
+
+        private void btnAplicarFiltros_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "" || txtApellido.Text != "" || txtDNI.Text != "")
+            {
+                employeeFilteredTable = createTableEmployee();
+
+                if (txtNombre.Text != "")
+                {
+                    var nombreIngresado = txtNombre.Text.ToUpper();
+                    for (int i = 0; i < datosEmpleados.Count; i++)
+                    {
+                        var nombreEmpleado = Convert.ToString((datosEmpleados[i].NOMBRE)).ToUpper();
+                        if (nombreEmpleado.Contains(nombreIngresado))
+                        {
+                            DataRow row = employeeFilteredTable.NewRow();
+                            row["DNI"] = datosEmpleados[i].DNI;
+                            row["NOMBRE"] = datosEmpleados[i].NOMBRE;
+                            row["APELLIDO"] = datosEmpleados[i].APELLIDO;
+                            employeeFilteredTable.Rows.Add(row);
+                        }
+                    }
+                }
+
+                if (txtApellido.Text != "")
+                {
+                    var apellidoIngresado = txtApellido.Text.ToUpper();
+                    for (int i = 0; i < datosEmpleados.Count; i++)
+                    {
+                        var apellidoEmpleado = Convert.ToString((datosEmpleados[i].APELLIDO)).ToUpper();
+                        if (apellidoEmpleado.Contains(apellidoIngresado))
+                        {
+                            DataRow row = employeeFilteredTable.NewRow();
+                            row["DNI"] = datosEmpleados[i].DNI;
+                            row["NOMBRE"] = datosEmpleados[i].NOMBRE;
+                            row["APELLIDO"] = datosEmpleados[i].APELLIDO;
+                            employeeFilteredTable.Rows.Add(row);
+                        }
+                    }
+                }
+
+                if (txtDNI.Text != "")
+                {
+                    var dniIngresado = txtDNI.Text;
+                    for (int i = 0; i < datosEmpleados.Count; i++)
+                    {
+                        var dniEmpleado = Convert.ToString((datosEmpleados[i].DNI));
+                        if (dniEmpleado.Contains(dniIngresado))
+                        {
+                            DataRow row = employeeFilteredTable.NewRow();
+                            row["DNI"] = datosEmpleados[i].DNI;
+                            row["NOMBRE"] = datosEmpleados[i].NOMBRE;
+                            row["APELLIDO"] = datosEmpleados[i].APELLIDO;
+                            employeeFilteredTable.Rows.Add(row);
+                        }
+                    }
+                }
+
+
+                dgvEmployees.DataSource = employeeFilteredTable;  
+            } else
+            {
+                MessageBox.Show("No se ingreso ningun filtro");
+            }
+
+        }
+
+        private void btnLimpiarCampos_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtDNI.Text = "";
+            dgvEmployees.DataSource = datosEmpleados;
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsLetter(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtDNI_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (e.KeyChar == (char)32)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNombre.Text.Length > 0)
+            {
+                activarLimpiarCampos();
+                txtApellido.Enabled = false;
+                txtDNI.Enabled = false;
+            }
+            else
+            {
+                desactivarLimpiarCampos();
+                txtApellido.Enabled = true;
+                txtDNI.Enabled = true;
+            }
+        }
+
+        private void txtApellido_TextChanged(object sender, EventArgs e)
+        {
+            if (txtApellido.Text.Length > 0)
+            {
+                activarLimpiarCampos();
+                txtNombre.Enabled = false;
+                txtDNI.Enabled = false;
+            }
+            else
+            {
+                desactivarLimpiarCampos();
+                txtNombre.Enabled = true;
+                txtDNI.Enabled = true;
+            }
+        }
+
+        private void txtDNI_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDNI.Text.Length > 0)
+            {
+                activarLimpiarCampos();
+                txtApellido.Enabled = false;
+                txtNombre.Enabled = false;
+            }
+            else
+            {
+                desactivarLimpiarCampos();
+                txtApellido.Enabled = true;
+                txtNombre.Enabled = true;
+            }
+        }
+
+        private void activarLimpiarCampos()
+        {
+            btnLimpiarCampos.BackgroundColor = ColorTranslator.FromHtml("#383C77");
+            btnLimpiarCampos.TextColor = Color.White;
+            btnLimpiarCampos.Enabled = true;
+        }
+
+        private void desactivarLimpiarCampos()
+        {
+            btnLimpiarCampos.BackgroundColor = Color.White;
+            btnLimpiarCampos.TextColor = Color.Black;
+            btnLimpiarCampos.Enabled = false;
+        }
+
     }
 }
