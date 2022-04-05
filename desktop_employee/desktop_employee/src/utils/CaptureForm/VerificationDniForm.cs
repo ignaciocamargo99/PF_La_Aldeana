4002153df1b6ae60a3bc753688278ef7886a8e6d
Joaquin Bassano <joaquinbas98@gmail.com>
2022-04-05 19:19:21 -0300

    US 706: nuevo form

---
+++ desktop_employee/desktop_employee/src/utils/CaptureForm/VerificationDniForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desktop_employee.src.utils.CaptureForm
+{
+    public partial class VerificationDniForm : Form
+    {
+        public VerificationDniForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
