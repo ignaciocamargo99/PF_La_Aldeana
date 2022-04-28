57f6b04830bd4e61a13e1f9078418dfcd02458de
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2022-04-28 13:58:53 -0300

    US 772: Test fechas

--- web_client/src/views/EmployeeAssistance/components/TablePagination/TablePagination.js
+++ web_client/src/views/EmployeeAssistance/components/TablePagination/TablePagination.js
@@ -54,9 +54,8 @@ const TablePagination = ({ columnsHeaders, currentElements, handleRead, handleEd
             setCurrentPage(1);
         }
         else {
-            let filteredNowDate = [];
-            if (PORT() === "") filteredNowDate = currentElements.filter((elem) => (moment(elem.date_entry).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')));
-            else filteredNowDate = currentElements.filter((elem) => (moment(elem.date_entry).subtract(1, 'days').format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')));
+            let filteredNowDate = currentElements.filter((elem) => (moment(elem.date_entry).format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')));
+            // else filteredNowDate = currentElements.filter((elem) => (moment(elem.date_entry).subtract(1, 'days').format('YYYY-MM-DD') === moment().format('YYYY-MM-DD')));
 
             setFilteredElements(filteredNowDate);
             setCurrentPage(1);
