c45acefe790855875edd8f66ba5675906cb8ba40
tamyer <mateo.nicolas.reyes@gmail.com>
2022-05-18 16:21:03 -0300

    US 634: pdf + estilo unitario para pdf

--- web_client/src/views/Reports/Salaries/components/Options.js
+++ web_client/src/views/Reports/Salaries/components/Options.js
@@ -154,11 +154,11 @@ console.log(props.dateFrom , props.dateTo , props.load)
             </div>
             <div className="formRow d-flex justify-content-between">
                 <div className="mx-auto">
-                    <BeShowed show={permissionsAccess === 1 || permissionsAccess === 2 || permissionsAccess === 'Reportes Recursos Humanos'}>
+                    <BeShowed show={permissionsAccess === 1 || permissionsAccess === 2}>
                         <button className="newBtn" id='genrateButon' style={{ marginRight: '1em', minWidth: '15em' }} onClick={handlerLoader}><FaAngleRight /> Generar informe</button>
                         <button className="disabledNewBtn" style={{ marginRight: '1em', minWidth: '15em' }} id='printButon' disabled><FaFile /> Imprimir informe</button>
                     </BeShowed>
-                    <BeShowed show={permissionsAccess === 3}>
+                    <BeShowed show={permissionsAccess === 3 || permissionsAccess === 'Reportes Recursos Humanos'}>
                         <button className="newBtn" id='genrateButon' style={{ marginRight: '1em', minWidth: '15em' }} onClick={handlerLoader}><FaAngleRight /> Generar informe</button>
                         <button className="newBtn" id='printButon' disabled={props.dateFrom > props.dateTo || props.load <= 0} style={props.dateFrom <= props.dateTo && props.load > 0 ? { minWidth: '15em' } : { minWidth: '15em', backgroundColor: 'grey' }}
                             onClick={showRenderPDF} ><FaFile /> Imprimir informe</button>
--- web_client/src/views/Reports/Salaries/components/PDFSalariesReport.js
+++ web_client/src/views/Reports/Salaries/components/PDFSalariesReport.js
@@ -1,92 +1,8 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet, Image } from '@react-pdf/renderer';
+import { Page, Text, View, Document, Image } from '@react-pdf/renderer';
 import dateFormat from '../../../../utils/DateFormat/dateFormat';
 import dataChartToURL from '../../../../utils/dataChartToURL';
-
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    backgroundColor: 'white',
-    paddingTop: 35,
-    paddingBottom: 65,
-    paddingHorizontal: 35,
-  },
-  sectionFace: {
-    margin: 10,
-    padding: 10,
-    marginTop: '35%'
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-  },
-  row: {
-    flexDirection: 'row',
-  },
-  col: {
-    borderWidth: '2px',
-    width: '1000em',
-    textAlign: 'center',
-    margin: '-1px',
-  },
-  col3: {
-    borderWidth: '2px',
-    textAlign: 'center',
-    margin: '-1px',
-    width: '500em',
-  },
-  col7: {
-    borderWidth: '2px',
-    textAlign: 'center',
-    margin: '-1px',
-    width: '80em',
-  },
-  col8: {
-    borderWidth: '2px',
-    textAlign: 'center',
-    margin: '-1px',
-    width: '40em',
-  },
-  title: {
-    fontSize: 18,
-    textAlign: 'center',
-    paddingBottom: 15,
-    paddingTop: 15,
-    color: 'grey',
-  },
-  mainTitle: {
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  text: {
-    fontSize: 10,
-  },
-  header: {
-    fontSize: 12,
-    marginBottom: 20,
-    textAlign: 'center',
-    color: 'grey',
-  },
-  subtitle: {
-    fontSize: 16,
-    paddingLeft: '3px',
-    paddingRight: '3px',
-    color: 'grey',
-  },
-  detail: {
-    fontSize: 12,
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-  image: {
-    marginVertical: 30,
-    marginHorizontal: 50,
-  },
-  logo: {
-    marginVertical: 10,
-    marginHorizontal: 150,
-  },
-});
+import styles from '../../styles';
 
 // Create Document Component
 export default function MyDocument (props) {
@@ -123,7 +39,7 @@ export default function MyDocument (props) {
                       <Text style={styles.text}>{element.id}</Text>
                     </View>
                     <View style={styles.col}>
-                      <Text style={styles.text}>$ {element.quantity}</Text>
+                      <Text style={styles.money}>$ {element.quantity}</Text>
                     </View>
                   </View>
                 </>
@@ -137,7 +53,7 @@ export default function MyDocument (props) {
                 <Text style={styles.subtitle}>Nombre</Text>
               </View>
               <View style={styles.col8}>
-                <Text style={styles.subtitle}>Hs. trabajadas/extras</Text>
+                <Text style={styles.subtitle}>Hs. trabajadas / extras</Text>
               </View>
               <View style={styles.col8}>
                 <Text style={styles.subtitle}>Recibo de sueldo</Text>
@@ -155,7 +71,7 @@ export default function MyDocument (props) {
                 <Text style={styles.subtitle}>Total</Text>
               </View>
             </View>
-            {props.sales?.map(element => {
+            {props.salaries?.map(element => {
               return (
                 <>
                   <View style={styles.row}>
@@ -163,22 +79,22 @@ export default function MyDocument (props) {
                       <Text style={styles.text}>{element.fullName}</Text>
                     </View>
                     <View style={styles.col8}>
-                      <Text style={styles.text}>$ {element.salary_hs}</Text>
+                      <Text style={styles.money}>$ {element.salary_hs}</Text>
                     </View>
                     <View style={styles.col8}>
-                      <Text style={styles.text}>$ {element.paycheck}</Text>
+                      <Text style={styles.money}>$ {element.paycheck}</Text>
                     </View>
                     <View style={styles.col8}>
-                      <Text style={styles.text}>$ {element.plus}</Text>
+                      <Text style={styles.money}>$ {element.plus}</Text>
                     </View>
                     <View style={styles.col8}>
-                      <Text style={styles.text}>$ {element.subtotal}</Text>
+                      <Text style={styles.money}>$ {element.subtotal}</Text>
                     </View>
                     <View style={styles.col8}>
-                      <Text style={styles.text}>$ {element.minus}</Text>
+                      <Text style={styles.money}>$ {element.minus}</Text>
                     </View>
                     <View style={styles.col8}>
-                      <Text style={styles.text}>$ {element.total}</Text>
+                      <Text style={styles.money}>$ {element.total}</Text>
                     </View>
                   </View>
                 </>
