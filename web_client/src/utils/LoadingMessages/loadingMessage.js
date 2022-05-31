1547e9119093e6bf41648b2164bddd10a4c66c32
Mati <matiasgiorda16@gmail.com>
2022-05-31 20:30:51 -0300

    BUG857: Merge staging

--- web_client/src/utils/LoadingMessages/loadingMessage.js
+++ web_client/src/utils/LoadingMessages/loadingMessage.js
@@ -1,30 +1,6 @@
 import swal from 'sweetalert';
 
 export default function loadingMessage(msg) {
-<<<<<<< HEAD
-    let loading = document.createElement('div')
-    let divSpinner = document.createElement('div')
-    divSpinner.className = "formRow justify-content-center"
-    let spinner1 = document.createElement('div')
-    spinner1.className = "spinner-grow text-success"
-    let spinner2 = document.createElement('div')
-    spinner2.className = "spinner-grow text-success"
-    let spinner3 = document.createElement('div')
-    spinner3.className = "spinner-grow text-success"
-    let divText = document.createElement('div')
-    let text = document.createElement('label')
-    text.innerText = msg
-    divText.appendChild(text)
-    divSpinner.appendChild(spinner1)
-    divSpinner.appendChild(spinner2)
-    divSpinner.appendChild(spinner3)
-    loading.appendChild(divSpinner)
-    loading.appendChild(divText)
-
-    return swal({ content: loading, buttons: false });
-
-}
-=======
     let loading = document.createElement('div');
     let divSpinner = document.createElement('div');
     divSpinner.className = 'formRow justify-content-center';
@@ -46,4 +22,3 @@ export default function loadingMessage(msg) {
 
     return swal({ content: loading, buttons: false });
 }
->>>>>>> staging
