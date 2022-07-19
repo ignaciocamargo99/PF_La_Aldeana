bdcdb937efe2e78fd6fa83dc0466596b69b7ed1f
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2022-07-19 14:56:20 -0300

    US 851: Corrección en endpoint de ventas

--- server/api/db/salesDb.js
+++ server/api/db/salesDb.js
@@ -1,7 +1,7 @@
 const pool = require('../../config/connection');
 
 const PayTypesGetDB = () => {
-    const sqlSelect = "SELECT * FROM PAY_TYPES"
+    const sqlSelect = 'SELECT * FROM PAY_TYPES';
 
     return new Promise((resolve, reject) => {
         pool.getConnection((error, db) => {
@@ -17,11 +17,12 @@ const PayTypesGetDB = () => {
 };
 
 const salePostDB = (newSale) => {
-    const { date_hour, total_amount, id_pay_type, details, cellphone_client } = newSale;
+    const { date_hour, total_amount, id_pay_type, details } = newSale;
     let id_sale;
     let arrDetails = JSON.parse(details);
 
     const selectMaxIdSale = 'SELECT MAX(id_sale) AS last_id_sale FROM SALES';
+    const sqlInsertSale = 'INSERT INTO SALES(id_sale, date_hour, total_amount, id_pay_type) VALUES(?, ?,?,?)';
 
     return new Promise((resolve, reject) => {
         pool.getConnection((error, db) => {
@@ -29,49 +30,22 @@ const salePostDB = (newSale) => {
 
             db.query(selectMaxIdSale, (error, row) => {
                 if (error) reject('1:' + error);
-
                 else id_sale = row[0].last_id_sale + 1;
-            })
+            });
             db.beginTransaction((error) => {
-                if (error) reject('1,5:' + error);
-                const sqlInsertSale = `INSERT INTO SALES(id_sale, date_hour, total_amount, id_pay_type) VALUES(${id_sale}, '${date_hour}',${total_amount},${id_pay_type})`;
-
-                db.query(sqlInsertSale, (error, result) => {
-                    if (error) {
-                        return db.rollback(() => reject('2:' + error))
-                    }
+                if (error) reject(error);
+                db.query(sqlInsertSale, [id_sale, date_hour, total_amount, id_pay_type], (error) => {
+                    if (error) return db.rollback(() => reject(error))
                     for (let i = 0; i < arrDetails.length; i++) {
-
                         const sqlInsertSaleDetailSale = `INSERT INTO DETAIL_SALES VALUES(${i + 1},${id_sale},${arrDetails[i].id_product},${arrDetails[i].quantity},${arrDetails[i].subtotal})`;
-
                         db.query(sqlInsertSaleDetailSale, (error) => {
-                            if (error) {
-                                console.log(error)
-                                return db.rollback(() => reject('3:' + error));
-                            }
-                            // if (arrDetails[i].listSupplies.length > 0){
-                            //     for (let j = 0; j < arrDetails[i].listSupplies.length; j++) {
-
-                            //         let menos = parseInt(arrDetails[i].quantity) * arrDetails[i].listSupplies[j][0];
-                            //         let resultado = arrDetails[i].listSupplies[j][1].stock_unit - parseInt(menos);
-                            //         const sqlUpdateSupply = `UPDATE SUPPLIES SET stock_unit=${resultado} WHERE id_supply=${arrDetails[i].listSupplies[j][1].id_supply}`;
-
-                            //         db.query(sqlUpdateSupply, (error) => {
-                            //             if (error) {
-                            //                 return db.rollback(() => reject('4:' + error));
-                            //             }    
-                            //             db.commit((error) => {
-                            //                 if (error) {
-                            //                     return db.rollback(() => reject('5:' + error));
-                            //                 }
-                            //                 else resolve();
-                            //             });  
-                            //         })        
-                            //     }
-                            // }
-                            else resolve();
+                            if (error) return db.rollback(() => reject(error));
+                            db.commit((error) => {
+                                if (error) db.rollback(() => reject(error));
+                                else resolve();
+                            });
                         });
-                    };
+                    }
                 });
                 db.release();
             });
@@ -156,4 +130,4 @@ const saleDeliveryPostDB = (newSale) => {
     });
 };
 
-module.exports = { PayTypesGetDB, salePostDB, saleDeliveryPostDB }
+module.exports = { PayTypesGetDB, salePostDB, saleDeliveryPostDB };
