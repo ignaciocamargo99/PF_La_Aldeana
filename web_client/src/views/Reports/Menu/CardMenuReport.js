2e56b009f131fda287a652df972edaa73a3e8c87
Fernando Cisneros <fcisneros@proofpoint.com>
2022-08-04 21:42:10 -0300

    fix css cards reportes

--- web_client/src/views/Reports/Menu/CardMenuReport.js
+++ web_client/src/views/Reports/Menu/CardMenuReport.js
@@ -1,17 +1,17 @@
 export default function CardMenuReport(props) {
-  return (
-    <>
-      <div style={{ display: "none" }}>
-        {(document.title = "Reportes de " + props.type)}
-      </div>
-      <div className="viewTitle">
-        <h1>Reportes de {props.type}</h1>
-      </div>
-      <div className="viewBody">
-        <div className="schedules-cards-container">
-          <div className="cards-container">{props.children}</div>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div style={{ display: "none" }}>
+                {(document.title = "Reportes de " + props.type)}
+            </div>
+            <div className="viewTitle">
+                <h1>Reportes de {props.type}</h1>
+            </div>
+            <div className="viewBody">
+                <div className="schedules-cards-container">
+                    {props.children}
+                </div>
+            </div>
+        </>
+    );
 }
