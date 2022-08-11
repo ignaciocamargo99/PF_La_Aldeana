0cd1a64c7fdbaba2348088c392ce79c7653051b6
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2022-08-10 23:53:11 -0300

    US Mejoras en matriz de permisos

--- web_client/src/views/Users/components/Permissions/SelectPermission.js
+++ web_client/src/views/Users/components/Permissions/SelectPermission.js
@@ -12,6 +12,7 @@ export default function CheckBoxEnabled(props) {
 
     useEffect(() => {
         if (props.data.editing && matrixLoad && props.permission) {
+            console.log('matrixLoad', matrixLoad)
             let permissions1 = [];
             let idPermission = [];
             permissions1 = props.permission.find((permission) => permission.id_permission === props.index + 1);
